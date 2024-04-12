ba24480eec081d1abba2fd9dec478cbaed6f2dcd
Harold Gómez <xagobo14@gmail.com>
2024-04-11 23:57:50 -0500

    cambio idioma landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,12 +36,12 @@
                         </a>
                         @else
                         <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                            Log in
+                            Iniciar Sesión
                         </a>
 
                         @if (Route::has('register'))
                         <a href="{{ route('register') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                            Register
+                            Registrarse
                         </a>
                         @endif
                         @endauth
