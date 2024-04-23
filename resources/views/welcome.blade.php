9472656e88daa5ce80aeb29747761cb7f169e8fc
Harold Gomez <xagobo14@gmail.com>
2024-04-23 03:23:19 -0500

    ajuste interlineado del header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                <header class="grid grid-cols-2 items-center gap-2 pb-40 lg:grid-cols-3">
 
                     <!-- Logo -->
                     <div class="flex lg:justify-center lg:col-start-2">
