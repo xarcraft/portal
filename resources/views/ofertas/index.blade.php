6d34ccc28386fa7801ad308839d039e9c1951c41
Harold Gómez <xagobo14@gmail.com>
2024-05-03 18:14:53 -0500

    confirmacion de oferta publicada

--- resources/views/ofertas/index.blade.php
+++ resources/views/ofertas/index.blade.php
@@ -7,6 +7,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if(session()->has('mensaje'))
+            <div class="uppercase border-green-600 bg-green-100 text-green-600 font-bold p-2 my-3 text-sm">
+                {{session('mensaje')}}
+            </div>
+            @endif
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("Ofertas Publicadas") }}
