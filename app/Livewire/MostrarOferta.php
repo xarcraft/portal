6b83920c1565888203e0b83d50a2cb9d1e350656
Harold Gómez <xagobo14@gmail.com>
2024-05-04 15:12:38 -0500

    ordenadas las publicaciones por fecha

--- app/Livewire/MostrarOferta.php
+++ app/Livewire/MostrarOferta.php
@@ -9,7 +9,7 @@ class MostrarOferta extends Component
 {
     public function render()
     {
-        $ofertas = Oferta::where('user_id', auth()->user()->id)->paginate(10);
+        $ofertas = Oferta::where('user_id', auth()->user()->id)->orderBy('updated_at','DESC')->paginate(10);
 
         return view('livewire.mostrar-oferta', [
             'ofertas' => $ofertas
