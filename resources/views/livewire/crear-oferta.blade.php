e40cf6c2a4a59c6b5f841398cbc812231db8369c
Harold Gómez <xagobo14@gmail.com>
2024-05-06 11:21:01 -0500

    se agregan comentarios a los diferentes campos

--- resources/views/livewire/crear-oferta.blade.php
+++ resources/views/livewire/crear-oferta.blade.php
@@ -1,26 +1,33 @@
 <form action="" class="md:w-1/2 space-y-5" wire:submit.prevent='crearOferta'>
-    <!-- Email Address -->
+    <!-- Titulo -->
     <div>
         <x-input-label for="titulo" :value="__('Nombre de la Oferta')" />
         <x-text-input id="titulo" class="block mt-1 w-full" type="text" wire:model="titulo" :value="old('titulo')" placeholder="Qué estás buscando?" />
         <x-input-error :messages="$errors->get('titulo')" class="mt-2" />
     </div>
+
+    <!-- Empresa -->
     <div>
         <x-input-label for="empresa" :value="__('Nombre de tu empresa')" />
         <x-text-input id="empresa" class="block mt-1 w-full" type="text" wire:model="empresa" :value="old('empresa')" placeholder="Quién esta buscando?" />
         <x-input-error :messages="$errors->get('empresa')" class="mt-2" />
     </div>
+
+    <!-- Ubicaión -->
     <div>
         <x-input-label for="ubicacion" :value="__('Locación')" />
         <x-text-input id="ubicacion" class="block mt-1 w-full" type="text" wire:model="ubicacion" :value="old('ubicacion')" placeholder="Dónde está ubicada la empresa?" />
         <x-input-error :messages="$errors->get('ubicacion')" class="mt-2" />
     </div>
+
+    <!-- Descripción -->
     <div>
         <x-input-label for="descripcion" :value="__('Descripción de la oferta')" />
         <textarea wire:model="descripcion" id="descripcion" placeholder="Describe brevemente tu oferta para los posibles interesados" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm h-72"></textarea>
         <x-input-error :messages="$errors->get('descripcion')" class="mt-2" />
     </div>
 
+    <!-- Salario -->
     <div>
         <x-input-label for="salario" :value="__('Salario mensual')" />
         <select wire:model="salario" id="salario" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" wire:model="salario">
@@ -32,6 +39,7 @@
         <x-input-error :messages="$errors->get('salario')" class="mt-2" />
     </div>
 
+    <!-- Modalidad -->
     <div>
         <x-input-label for="modalidad" :value="__('Modalidad de trabajo')" />
         <select wire:model="modalidad" id="modalidad" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" wire:model="modalidad">
@@ -42,11 +50,15 @@
         </select>
         <x-input-error :messages="$errors->get('modalidad')" class="mt-2" />
     </div>
+
+    <!-- Requerimientos -->
     <div>
         <x-input-label for="requerimientos" :value="__('Requerimientos')" />
         <textarea wire:model="requerimientos" id="requerimientos" placeholder="Digite aquí si su oferta tiene requerimientos específicos (opcional)" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm h-52"></textarea>
         <x-input-error :messages="$errors->get('requerimientos')" class="mt-2" />
     </div>
+
+    <!-- Horario -->
     <div>
         <x-input-label for="horario" :value="__('Horario')" />
         <select wire:model="horario" id="horario" name="horario" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
@@ -57,6 +69,8 @@
         </select>
         <x-input-error :messages="$errors->get('horario')" class="mt-2" />
     </div>
+
+    <!-- Imagen -->
     <div>
         <x-input-label for="imagen" :value="__('Promoción gráfica de la oferta (opcional)')" />
         <x-text-input id="imagen" class="block mt-1 w-full" type="file" wire:model="imagen" accept='image/*' />
