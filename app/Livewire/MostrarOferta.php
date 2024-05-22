007698dac03bb1b22ebff88d2e74ff81686fc7b1
Harold Gómez <xagobo14@gmail.com>
2024-05-22 11:14:17 -0500

    cambio de vista de ofertas solo a las ya revisadas

--- app/Livewire/MostrarOferta.php
+++ app/Livewire/MostrarOferta.php
@@ -22,7 +22,7 @@ class MostrarOferta extends Component
 
     public function render()
     {
-        $ofertas = Oferta::where('user_id', auth()->user()->id)->orderBy('updated_at', 'DESC')->paginate(10);
+        $ofertas = Oferta::where([['user_id', auth()->user()->id], ['revision', '1']])->orderBy('updated_at', 'DESC')->paginate(10);
 
         return view('livewire.mostrar-oferta', [
             'ofertas' => $ofertas
