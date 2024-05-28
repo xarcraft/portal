7691670616598cdc5a24691dabd33c9c49de22f1
Harold Gómez <xagobo14@gmail.com>
2024-05-28 10:11:48 -0500

    creacion de un tema personalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,38 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
+        // colors: {
+        //     transparent: "transparent",
+        //     current: "currentColor",
+        //     white: "#ffffff",
+        //     stratos: {
+        //         50: "#e4efff",
+        //         100: "#cfe0ff",
+        //         200: "#a8c4ff",
+        //         300: "#749dff",
+        //         400: "#3e61ff",
+        //         500: "#1328ff",
+        //         600: "#0010ff",
+        //         700: "#0010ff",
+        //         800: "#000ee4",
+        //         900: "#0005b0",
+        //         950: "#000035",
+        //     },
+        // },
     },
 
     plugins: [forms],
