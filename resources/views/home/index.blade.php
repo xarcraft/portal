578a06715452797c7ca309096619a3817dd26cf4
Harold Gómez <xagobo14@gmail.com>
2024-05-30 20:48:36 -0500

    cambios fuente

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,7 +2,7 @@
     <div class="py-16 bg-gray-50 overflow-hidden lg:py-24">
         <div class=" max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
             <div class="relative">
-                <h2 class="text-center text-4xl leading-8 font-extrabold tracking-tight text-blue-800 sm:text-6xl">S.i Nova Gestión: Uniendo Talentos y Oportunidades</h2>
+                <h2 class="text-center text-4xl leading-8 font-extrabold tracking-tight text-blue-800 sm:text-6xl">S.I Nova Gestión: Uniendo Talentos y Oportunidades</h2>
                 <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">Descubre oportunidades laborales que se ajustan a tus habilidades y ambiciones. <br><span class="font-bold">¡Tu futuro comienza aquí!</span></p>
             </div>
         </div>
